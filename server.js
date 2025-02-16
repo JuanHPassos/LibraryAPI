e7637666f0c00d2102350b0de11632d3c2c0097a
JuanHPassos <juan12.henriques@gmail.com>
2025-02-16 19:33:38 -0300

    Add routes and nodemon

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ import http from "http";
 
 const PORT = 3000;
 
+const routes = {
+    // Basic route
+    "/": "Curse of Node.js",
+    "/books": "Entered the books route",
+    "/authors": "Entered the authors route"
+}; 
+
 const server = http.createServer((req, res) => {
     // Header response and the content type
     res.writeHead(200, { "Content-Type": "text/plain" });
     // End of the response
-    res.end("Curse of Node.js");
+    res.end(routes[req.url]);
 });
 
 // Waiting event at PORT
