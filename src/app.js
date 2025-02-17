d71edcbc5b39a95269430887e8432ebe65f637f6
JuanHPassos <juan12.henriques@gmail.com>
2025-02-17 08:24:05 -0300

    Add books route

--- src/app.js
+++ src/app.js
@@ -3,9 +3,24 @@ import express from "express";
 // Initializes an Express server
 const app = express();
 
-// Manages routes
+const books = [
+    {
+        id: 1,
+        title: "The lord of the rings"
+    },
+    {
+        id: 2,
+        title: "The Hobbit"
+    }
+]
+
+// Manages routes (using GET method/request)
 app.get("/", (req, res) => {
     res.status(200).send("Curse of Node.js");
+});
+
+app.get("/books", (req, res) => {
+    res.status(200).json(books);
 });
 
 export default app;
